8c103d001b650fc169f3d9eda27f14db9b786fb5
LEKTO <149956083+lektoflasher@users.noreply.github.com>
2025-09-11 01:41:10 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "livetime.worker.dev"; // Ganti dengan domain utama kalian
-const serviceName = "prrivatevpn"; // Ganti dengan nama workers kalian
+const serviceName = "privatevpn"; // Ganti dengan nama workers kalian
 const apiKey = "05e2fead943340082b40dff507198b284a14c"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = "sis1733@gmail.com"; // Ganti dengan email yang kalian gunakan
 const accountID = "337c37587f708748ebc8022d43427206"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
